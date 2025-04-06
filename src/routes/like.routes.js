10594b49cfb393ae4439e9d231421162537e46e7
Moazam-khan <moazamkhan8999@gmail.com>
2025-04-06 13:17:13 +0500

    Likes Complete

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -4,15 +4,24 @@ import {
     toggleCommentLike,
     toggleVideoLike,
     toggleTweetLike,
-} from "../controllers/like.controller.js"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
+} from "../controllers/like.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
-router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
+// Apply verifyJWT middleware to protect all routes
+router.use(verifyJWT);
+
+// Route to toggle like on a video
 router.route("/toggle/v/:videoId").post(toggleVideoLike);
+
+// Route to toggle like on a comment
 router.route("/toggle/c/:commentId").post(toggleCommentLike);
+
+// Route to toggle like on a tweet
 router.route("/toggle/t/:tweetId").post(toggleTweetLike);
+
+// Route to get all liked videos
 router.route("/videos").get(getLikedVideos);
 
-export default router
+export default router;
